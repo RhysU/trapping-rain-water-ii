86bea50f0911171300607af4a85ba47eb2775adb
Rhys Ulerich <rhys.ulerich@gmail.com>
2020-06-07 15:19:42 -0400

    Draft

--- impl.c
+++ impl.c
@@ -19,66 +19,92 @@
 //    H[i][j] represents the terrain elevation (i.e. height)
 //    W[i][j] represents the water atop the terrain
 //
-//  No claims of optimal anything on the solution.
+//  Implementation is gross, gross, gross.
+//  There are smart linear programming algorithms for this stuff.
 static
-int impl(int** H, int m, int n)
+int
+impl(int** H, int m, int n)
 {
+    // Left hand side data, as computed below
+    int* Ldat = calloc(4 * m * n, sizeof(int));
+
+    // Index of positive contribution to constraints.
+    ptrdiff_t* Rpos  = calloc(4 * m * n, sizeof(ptrdiff_t));
+
+    // Unknown water height
+    int* W = calloc(m * n, sizeof(int));
+
+    // Tracks maximum height
+    int hmax = 0;
+
     // Constraints like...
     //     H[i][j] + w[i][j] <= H[i+1][j] + w[i+1][j]
     // ...are rearranged into...
     //     H[i][j] - H[i+1][j] <= w[i+1][j] - w[i][j]
-    //     H[i][j] - H[i-1][j] <= w[i-1][j] - w[i][j]
-    //     H[i][j] - H[i][j+1] <= w[i][j+1] - w[i][j]
-    //     H[i][j] - H[i][j-1] <= w[i][j-1] - w[i][j]
     // ...so a single-pass on the H matrix finds left hand sides.
     //
     // During that pass, compute offsets to right hand side data.
     //
-    // Afterwards, compute an upper bound on the possible water W.
-    int* Ldat = calloc(4 * m * n, sizeof(int));
-    ptrdiff_t* Rpos  = calloc(4 * m * n, sizeof(ptrdiff_t));
-    ptrdiff_t* Rneg  = calloc(4 * m * n, sizeof(ptrdiff_t));
-    int* W = calloc(m * n, sizeof(int));
+    // This processing completes all ingest of incoming data.
     {
-        int hmax = 0;
         int off = 0;
         for (int i = 1; i < m-1; ++i) {
             for (int j = 1; j < n-1; ++j) {
                 // H[i][j] - H[i+1][j] <= w[i+1][j] - w[i][j]
                 Ldat[off  ] = H[i][j] - H[i+1][j];
-                Rpos[off  ] = (i+1)*m + (j);
-                Rneg[off++] = i*m + j;
+                Rpos[off++] = (i+1)*m + (j);
 
                 // H[i][j] - H[i-1][j] <= w[i-1][j] - w[i][j]
                 Ldat[off  ] = H[i][j] - H[i-1][j];
-                Rpos[off  ] = (i-1)*m + (j);
-                Rneg[off++] = i*m + j;
+                Rpos[off++] = (i-1)*m + (j);
 
                 // H[i][j] - H[i][j+1] <= w[i][j+1] - w[i][j]
                 Ldat[off  ] = H[i][j] - H[i][j+1];
-                Rpos[off  ] = (i)*m + (j+1);
-                Rneg[off++] = i*m + j;
+                Rpos[off++] = (i)*m + (j+1);
 
                 // H[i][j] - H[i][j-1] <= w[i][j-1] - w[i][j]
                 Ldat[off  ] = H[i][j] - H[i][j-1];
-                Rpos[off  ] = (i)*m + (j-1);
-                Rneg[off++] = i*m + j;
+                Rpos[off++] = (i)*m + (j-1);
 
                 // Observed the maximum height
                 if (H[i][j] > hmax) hmax = H[i][j];
             }
         }
 
-        // The maximum water in any location is no more than maximum height
-        for (int i = 1; i < m-1; ++i) {
-            for (int j = 1; j < n-1; ++j) {
-                W[i*m + j] = hmax - H[i][j];
+    }
+
+    // Initialize water height to the overall maximum elevation
+    for (int i = 1; i < m-1; ++i) {
+        for (int j = 1; j < n-1; ++j) {
+            W[i*m + j] = hmax - H[i][j];
+        }
+    }
+
+    // Now, drain away water whenever constraints are not satisfied.
+    // Indices i, j, k take on new meanings below versus that from above.
+    for (int i = hmax; i --> 0;) {
+        for (int j = 0; j < m * n; ++j) {
+            for (int k = 0; k < 4; ++k) {
+                if (Ldat[4*j+k] > W[Rpos[j]] - W[j]) {  // Violated?
+                    W[j] = W[j] > 0 ? --W[j] : 0;       // Yes, drain.
+                    break;                              // Next square.
+                }
             }
         }
     }
 
-    return 0;
+    // How much water remains?
+    int sum = 0;
+    for (int j = 0; j < m * n; ++j) sum += W[j];
+
+    // Clean up
+    free(W);
+    free(Rpos);
+    free(Ldat);
+
+    return sum;
 }
+
 
 // Possibly ragged array implementation delegates to non-ragged variant.
 int trapRainWater(int** H, int m, int* n)
